bcf26eb64290cc824fa9a3a065b2764d05a4c263
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2021-07-04 00:42:55 +0530

    Added primecheck prograam

---
+++ Programs/primecheck.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int res, num, count = 0;
+    printf("Enter number to check prime or not prime:\n");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+    {
+        res = num % i;
+        if (res == 0)
+            count++;
+    }
+    if (count == 2)
+        printf("%d is a prime number\n", num);
+    else
+        printf("%d is not a prime number\n", num);
+    return 0;
+}
