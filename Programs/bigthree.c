45e3bea7aa958109c6dd54e2a4f728cc337abef1
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2021-08-07 00:03:38 +0530

    Edited bigthree.c

--- Programs/bigthree.c
+++ Programs/bigthree.c
@@ -6,6 +6,6 @@ void main()
     int a, b, c;
     printf("Enter three numbers:\n");
     scanf("%d %d %d", &a, &b, &c);
-    (a > b) ? ((a > c) ? printf("%d is Biggest", a) : printf("%d is Biggest", c)) : (b > c) ? printf("%d is Biggest", b)
-                                                                                            : printf("%d is Biggest", c);
+    (a > b) ? ((a > c) ? printf("%d is Biggest\n", a) : printf("%d is Biggest\n", c)) : (b > c) ? printf("%d is Biggest\n", b)
+                                                                                            : printf("%d is Biggest\n", c);
 }
